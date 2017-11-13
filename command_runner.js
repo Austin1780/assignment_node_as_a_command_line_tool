c26690c05dcaa6071be5e2dc58c896a2165982b6
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-13 14:32:59 -0700

    working on command_runner

--- command_runner.js
+++ command_runner.js
@@ -1,25 +1,28 @@
 var cp = require('child_process');
 
 process.stdin.on('data', function(input) {
-  input = String(input);
-  let [cmd, ...args] = input.split(' ');
+  if (input === 'q' || input === 'quit') {
+    process.exit();
+  } else {
+    input = String(input);
+    let [cmd, ...args] = input.split(' ');
 
-  var cmd1 = cp.spawn(cmd, args);
+    var cmd1 = cp.spawn(cmd, args);
 
-  cmd1.on('error', (err) => {
-    console.error(`${ err.stack }`);
-  });
+    cmd1.on('error', (err) => {
+      console.error(`${ err.stack }`);
+    });
 
-  cmd1.stdout.on('data', (data) => {
-    console.log(`Data: ${ data }`);
-  });
+    cmd1.stdout.on('data', (data) => {
+      console.log(`Data: ${ data }`);
+    });
 
-  cmd1.stderr.on('data', (data) => {
-    console.error(`STDERR: ${ data }`);
-  });
+    cmd1.stderr.on('data', (data) => {
+      console.error(`STDERR: ${ data }`);
+    });
 
-  cmd1.on('close', (code) => {
-    console.log(`Child process exited with code: ${ code }`);
-  });
-
+    cmd1.on('close', (code) => {
+      console.log(`Child process exited with code: ${ code }`);
+    });
+  }
 });
